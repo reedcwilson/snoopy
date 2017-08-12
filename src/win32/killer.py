c57a3dead9d0b19f58b9759f40becd47417a2cbc
Reed Wilson <reedcwilson@gmail.com>
2017-08-11 22:29:46 -0600

    don't alert on certain errors

--- src/win32/killer.py
+++ src/win32/killer.py
@@ -5,6 +5,18 @@ import zerorpc
 import traceback
 import logging
 
+known_errors = [
+    "An attempt was made to reference a token that does not exist"
+]
+
+
+def known_error(e):
+    message = str(e)
+    for err in known_errors:
+        if err in message:
+            return True
+    return False
+
 
 class GracefulKiller:
     def __init__(self, daemon, port):
@@ -19,10 +31,11 @@ class GracefulKiller:
             logging.info("Shutting down...")
             self.notifier.send(subject="Shutting down...")
         except Exception as e:
-            self.notifier.send(
-                subject="Alert!",
-                message="An unexpected error occurred: {}".format(
-                    traceback.format_exc()))
+            if not known_error(e):
+                self.notifier.send(
+                    subject="Alert!",
+                    message="An unexpected error occurred: {}".format(
+                        traceback.format_exc()))
         self.exit()
 
     def exit(self):
