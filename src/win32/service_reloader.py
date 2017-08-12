9e7ad4d3995f1c75c56329f54221711601139453
Reed Wilson <reedcwilson@gmail.com>
2017-08-11 22:00:17 -0600

    combine nssmlauncher with reloader for ease of use

--- src/win32/service_reloader.py
+++ src/win32/service_reloader.py
@@ -1,7 +1,57 @@
 import zerorpc
-from win32.launcher import NssmLauncher
+import subprocess
 import threading
 import time
+
+# from win32.launcher import NssmLauncher
+
+
+class NssmLauncher(object):
+    def __init__(self, nssm_file, service, directory, exe_args):
+        self.nssm_file = nssm_file
+        self.service = service
+        self.directory = directory
+        self.exe_args = exe_args
+
+    def nssm(self, args):
+        cmd = [self.nssm_file]
+        cmd.extend(args)
+        return subprocess.check_output(cmd).decode()
+
+    def nssm_set(self, args):
+        cmd = ['set', self.service]
+        cmd.extend(args)
+        self.nssm(cmd)
+
+    def get_status(self):
+        try:
+            status = self.nssm(['status', self.service])
+            return status.replace('\0', '').strip()
+        except subprocess.CalledProcessError:
+            return 'NOT_LOADED'
+
+    def stop(self):
+        self.nssm(['stop', self.service])
+
+    def remove(self):
+        subprocess.check_output(['sc', 'delete', self.service])
+
+    def install(self):
+        args = [
+            'install',
+            self.service
+        ]
+        args.extend(self.exe_args)
+        self.nssm(args)
+        self.nssm_set(['AppStopMethodSkip', '6'])
+        self.nssm_set(['AppDirectory', '{}'.format(self.directory)])
+        self.nssm_set(['AppStdout', r'{}\{}.out'.format(
+            self.directory, self.service)])
+        self.nssm_set(['AppStderr', r'{}\{}.err'.format(
+            self.directory, self.service)])
+
+    def start(self):
+        self.nssm(['start', self.service])
 
 
 class ServiceReloader(object):
