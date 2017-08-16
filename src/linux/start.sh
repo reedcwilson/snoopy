9c5af6cad624307294c32c27bb0ed32b36263fee
Reed Wilson <reedcwilson@gmail.com>
2017-08-16 10:46:41 -0600

    fix install

--- src/linux/start.sh
+++ src/linux/start.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo cp $1 /lib/systemd/system/
+DIR="$(dirname "$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")")"
+sed "s|%INSTALL_DIR%|${DIR}|g" "$1" > temp
+sudo cp temp "/lib/systemd/system/$2"
 sudo systemctl enable $2
 sudo systemctl start $2
+rm temp
